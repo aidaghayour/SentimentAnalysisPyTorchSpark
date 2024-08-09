954a158e098de9ffba48480bc1527d3d958a0837
aidaghayour <aghayourkhia@mun.ca>
2024-08-08 20:24:25 -0400

    BERT pretrained model and tokenizer intitated

---
+++ sentiment_model.py
@@ -0,0 +1,26 @@
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# model.to(device)
+
+# Load the tokenizer and model
+model_name = 'bert-base-uncased'
+tokenizer = BertTokenizer.from_pretrained(model_name)
+model = BertForSequenceClassification.from_pretrained(model_name)
+
+
+# Function to preprocess input and make predictions
+def predict_sentiment(text):
+    # Tokenize and encode the input text
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+    
+    # Make prediction
+    with torch.no_grad():
+        outputs = model(**inputs)
+    
+    # Get the predicted class
+    logits = outputs.logits
+    predicted_class = torch.argmax(logits, dim=1).item()
+    
+    return predicted_class
